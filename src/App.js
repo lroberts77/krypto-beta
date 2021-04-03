2b0afb1d922e8026b36292878d01dc80d02683c0
Liam Roberts <liamroberts77@googlemail.com>
2021-04-04 00:47:37 +0100

    installed axios and console logged response from api using axios

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import './App.css';
-import express from 'express'
+import axios from 'axios';
 
 function App() {
+
+  const [coins, setCoins] = useState([])
+
+  useEffect(() => {
+    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(res => {
+      setCoins(res.data)
+      console.log(res.data);
+    })
+  })
+
   return (
     <div>
       <h1>KRYPT</h1>
