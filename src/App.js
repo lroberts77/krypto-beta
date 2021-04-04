0cfb983f198797d5e4349ae04da0f8a2cca2e926
Liam Roberts <liamroberts77@googlemail.com>
2021-04-04 21:01:40 +0100

    added price change percentage 24h to Coin return in app.js

--- src/App.js
+++ src/App.js
@@ -8,16 +8,6 @@ function App() {
   const [coins, setCoins] = useState([])
   const [search, setSearch] = useState('')
 
-  // useEffect(() => {
-  //   axios.get(
-  //     'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
-  //     .then(res => {
-  //     setCoins(res.data)
-  //     console.log(res.data);
-  //   })
-  //   .catch(error => console.log(error));
-  // }, []);
-
   useEffect(() => {
     fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .then(res => {
@@ -53,6 +43,7 @@ function App() {
               symbol={coin.symbol} 
               price={coin.current_price}
               volume={coin.market_cap}
+              priceChange={coin.price_change_percentage_24h}
             />
           );
         })}
--- src/Coin.js
+++ src/Coin.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-const Coin = ({image, name, symbol, price, volume }) => {
+const Coin = ({image, name, symbol, price, volume, priceChange }) => {
     return (
         <div className="coin-cointainer">
             <div className="coin-row">
@@ -10,8 +10,13 @@ const Coin = ({image, name, symbol, price, volume }) => {
                     <p className="coin-symbol">{symbol}</p>
                 </div>
                 <div className="coin-data">
-                    <p className="coin-price">${price}</p>
+                    <p className="coin-price">${price.toLocaleString()}</p>
                     <p className="coin-volume">${volume.toLocaleString()}</p>
+                    {priceChange < 0 ? (
+                        <p className="coin-percent red">{priceChange.toFixed(2)}%</p>
+                    ) : (
+                        <p className="coin-percent green">{priceChange.toFixed(2)}%</p>
+                    )}
                 </div>
             </div>
         </div>
