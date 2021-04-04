c6e48e950dadb154ae0c86b792aa2325a9d7e449
Liam Roberts <liamroberts77@googlemail.com>
2021-04-04 12:08:10 +0100

    changed api call from axios to javascript for learning exposure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
-import axios from 'axios';
+// import axios from 'axios';
 
 function App() {
 
   const [coins, setCoins] = useState([])
 
+  // useEffect(() => {
+  //   axios.get(
+  //     'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+  //     .then(res => {
+  //     setCoins(res.data)
+  //     console.log(res.data);
+  //   })
+  //   .catch(error => console.log(error));
+  // }, []);
+  
   useEffect(() => {
-    axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false').then(res => {
-      setCoins(res.data)
-      console.log(res.data);
-    }).catch(error => console.log(error);)
-  })
+    fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
+    .then(res => {
+      res.json()
+    .then(data => console.log(data));
+    })
+    .catch(error => console.log(error));
+    }, []);
 
   return (
-    <div>
-      <h1>KRYPT</h1>
+    <div className="coin-app">
+      
     </div>
   );
 }
