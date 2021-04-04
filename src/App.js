82751880f0c322f2f6747cec4a4022ae993a818f
Liam Roberts <liamroberts77@googlemail.com>
2021-04-04 13:07:44 +0100

    added form to search a currency

--- src/App.js
+++ src/App.js
@@ -15,18 +15,25 @@ function App() {
   //   })
   //   .catch(error => console.log(error));
   // }, []);
-  
+
   useEffect(() => {
     fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false')
     .then(res => {
       res.json()
-    .then(data => console.log(data));
+      .then(data => console.log(data))
+      .then(data => setCoins(data));
     })
     .catch(error => console.log(error));
     }, []);
 
   return (
     <div className="coin-app">
+      <div className="coin-search">
+        <h1 className="coin-text">Search a currency</h1>
+        <form>
+          <input type="text" placeholder="Search..." className="coin-input"/>
+        </form>
+      </div>
       
     </div>
   );
