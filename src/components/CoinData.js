3d85d9e33807180144b21c38811039da38d35dfc
Liam Roberts <liamroberts77@googlemail.com>
2021-05-06 22:10:47 +0100

    added total volume to coindata.js

--- src/components/CoinData.js
+++ src/components/CoinData.js
@@ -7,6 +7,7 @@ const CoinData = ({ data }) => {
         <div>
               <span>Market Cap{data.market_cap}</span>
               <span>Total Supply{data.total_supply}</span>
+              <span>Total Volume{data.total_volume}</span>
         </div>
     )
 }
