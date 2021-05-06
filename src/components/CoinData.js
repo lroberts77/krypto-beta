174c91549ec10ff8704f290e8876603475f8662d
Liam Roberts <liamroberts77@googlemail.com>
2021-05-06 22:11:22 +0100

    added 24hr high to coindata.js

--- src/components/CoinData.js
+++ src/components/CoinData.js
@@ -8,6 +8,7 @@ const CoinData = ({ data }) => {
               <span>Market Cap{data.market_cap}</span>
               <span>Total Supply{data.total_supply}</span>
               <span>Total Volume{data.total_volume}</span>
+              <span>24hr High{data.high_24h}</span>
         </div>
     )
 }
