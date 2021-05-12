9a3ece56233b550084bf0a6fd1a298d7bfcd0b9d
Liam Roberts <liamroberts77@googlemail.com>
2021-05-12 19:36:43 +0100

    added column1 to data-container in coindata.js

--- src/components/CoinData.js
+++ src/components/CoinData.js
@@ -6,12 +6,14 @@ const CoinData = ({ data }) => {
     if (data) {
       return (
         <div className="data-container">
-        <span>Market Cap{data.market_cap}</span>
-        <span>Total Supply{data.total_supply}</span>
-        <span>Total Volume{data.total_volume}</span>
-        <span>24hr High{data.high_24h}</span>
-        <span>24hr Low{data.low_24h}</span>
-        <span>Circulating Supply{data.circulating_supply}</span>
+            <div className="column1">
+                <span>Market Cap{data.market_cap}</span>
+                <span>Total Supply{data.total_supply}</span>
+            </div>
+                <span>Total Volume{data.total_volume}</span>
+                <span>24hr High{data.high_24h}</span>
+                <span>24hr Low{data.low_24h}</span>
+                <span>Circulating Supply{data.circulating_supply}</span>
         </div>
       );
     }
