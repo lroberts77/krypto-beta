64a2fa3cb97d79b072c9f7f0c5dfd363457a802d
Liam Roberts <liamroberts77@googlemail.com>
2021-05-12 15:35:15 +0100

    added extra coin data to coindata.js

--- src/components/CoinData.js
+++ src/components/CoinData.js
@@ -1,20 +1,22 @@
-import React from 'react';
+import React from "react";
 
 const CoinData = ({ data }) => {
-    const renderData = () => {
-    // console.log(data);
-    return (
+  const renderData = () => {
+    if (data) {
+      return (
         <div>
-              <span>Market Cap{data.market_cap}</span>
-              <span>Total Supply{data.total_supply}</span>
-              <span>Total Volume{data.total_volume}</span>
-              <span>24hr High{data.high_24h}</span>
-              <span>24hr Low{data.low_24h}</span>
-              <span>Circulating Supply{data.circulating_supply}</span>
+        <span>Market Cap{data.market_cap}</span>
+        <span>Total Supply{data.total_supply}</span>
+        <span>Total Volume{data.total_volume}</span>
+        <span>24hr High{data.high_24h}</span>
+        <span>24hr Low{data.low_24h}</span>
+        <span>Circulating Supply{data.circulating_supply}</span>
         </div>
-    )
-}
-return <div>{renderData()}</div>;
-}
+      );
+    }
+  };
+
+  return <div>{renderData()}</div>;
+};
 
 export default CoinData;
