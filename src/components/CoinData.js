cfd203fc25bf5c7e97a161836e44e22fffb6a8d0
Liam Roberts <liamroberts77@googlemail.com>
2021-05-13 20:01:37 +0100

    formatted data values and edited styling in data-container in coindata.js

--- src/components/CoinData.js
+++ src/components/CoinData.js
@@ -9,31 +9,31 @@ const CoinData = ({ data }) => {
             <div className="column">
                 <div className="row">
                     <div className="detail-title">Market Cap</div>
-                    <div>{data.market_cap}</div>
+                    <div>${data.market_cap.toLocaleString()}</div>
                 </div>
                 <div className="row">
                     <div className="detail-title">Total Supply</div>
-                    <div>{data.total_supply}</div>
+                    <div>{data.total_supply.toLocaleString()}</div>
                 </div>
             </div>
             <div className="column">
                 <div className="row">
                     <div className="detail-title">Total Volume</div>
-                    <div>{data.total_volume}</div>
+                    <div>{data.total_volume.toLocaleString()}</div>
                 </div>
                 <div className="row"> 
-                    <div className="detail-title">24hr High</div>
-                    <div>{data.high_24h}</div>
+                    <div className="detail-title">Circulating Supply</div>
+                    <div>${data.circulating_supply.toLocaleString()}</div>
                 </div>
             </div>
             <div className="column">
                 <div className="row"> 
-                    <div className="detail-title">24hr Low</div>
-                    <div>{data.low_24h}</div>
+                    <div className="detail-title">24hr High</div>
+                    <div>${data.high_24h}</div>
                 </div>
                 <div className="row"> 
-                    <div className="detail-title">Circulating Supply</div>
-                    <div>{data.circulating_supply}</div>
+                    <div className="detail-title">24hr Low</div>
+                    <div>${data.low_24h}</div>
                 </div>
             </div>
         </div>
