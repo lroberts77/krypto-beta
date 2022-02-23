37034d294200b3cd434a04d4346f59d0540de196
Liam R <liamroberts77@googlemail.com>
2022-02-23 15:24:01 +0000

    fixed crypto cards layout in mid screen sizes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Link } from "react-router-dom";
 import './App.css';
 import CoinSummary from './pages/CoinSummary';
 import CoinDetail from './pages/CoinDetail';
@@ -10,7 +10,9 @@ export default function App() {
     <div className="coin-app">
       <BrowserRouter>
       <div className="coin-search">
-        <h1 className="coin-text">Krypto-Beta</h1>
+        <h1 className="coin-text">
+          <Link className='title' to='../' >Krypto-Beta</Link>
+          </h1>
       </div>
       <Route exact path="/" component={CoinSummary} />
       <Route path="/coins/:id" component={CoinDetail} />
